62e369d93bad8c31064ade7a7583065bbcd5f6f0
Tuiyeuoanh1304 <117872813+Tuiyeuoanh1304@users.noreply.github.com>
2022-11-11 10:00:23 +0700

    Update love.js

--- js/love.js
+++ js/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Gloria Hallelujah";
-            ctx.fillText("Come Baby", 23, 10);
+            ctx.fillText("Nhấn Vào Đây Đi :33", 23, 10);
             ctx.restore();
         },
         clear: function() {
