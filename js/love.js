7f49f920710b17972e30ade6e6bd872a6e3ccc18
Tuiyeuoanh1304 <117872813+Tuiyeuoanh1304@users.noreply.github.com>
2022-11-11 11:00:09 +0700

    Update love.js

--- js/love.js
+++ js/love.js
@@ -168,7 +168,7 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Gloria Hallelujah";
+            ctx.font = "12px Noto Sans,Caveat";
             ctx.fillText("Nhấn Đây Nè", 23, 10);
             ctx.restore();
         },
